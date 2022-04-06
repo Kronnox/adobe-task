51127b576ec70f67b974f7e7f779cabf5df0260b
Kronox <dev@kronox.me>
2022-04-06 13:53:39 +0200

    initial project structure

---
+++ src/main/java/de/lucaadams/adobetask/AdobeTaskApplication.java
@@ -0,0 +1,13 @@
+package de.lucaadams.adobetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdobeTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdobeTaskApplication.class, args);
+	}
+
+}
